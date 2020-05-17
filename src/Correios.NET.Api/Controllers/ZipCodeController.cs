39e1bdd77a8f6005dd38a8562c4d59ed265ca970
Wellington Rocha Parra <wrparra@wrsoft.com.br>
2020-05-17 13:23:12 -0300

    [UPDATE] Adicionado o projeto de Exemplo Correios.NET.Api para testar pacote nuget

---
+++ src/Correios.NET.Api/Controllers/ZipCodeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Correios.NET.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Correios.NET.Api.Controllers
+{
+    [ApiController]
+    [Route("cep")]
+    public class ZipCodeController : ControllerBase
+    {
+        private readonly Correios.NET.IServices _services;
+        private readonly ILogger<ZipCodeController> _logger;
+
+        public ZipCodeController(Correios.NET.IServices services, ILogger<ZipCodeController> logger)
+        {
+            _services = services;
+            _logger = logger;
+        }
+
+        [Route("{zipCode}")]
+        [HttpGet]
+        public async Task<IEnumerable<Correios.NET.Models.Address>> Get(string zipCode)
+        {
+            return await _services.GetAddressesAsync(zipCode);
+        }
+    }
+}
