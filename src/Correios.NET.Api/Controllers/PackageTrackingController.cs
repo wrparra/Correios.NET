f6a6d5a37211f2136b5f277bbe938d6c0b59d648
Wellington Rocha Parra <wrparra@wrsoft.com.br>
2020-06-01 12:29:08 -0300

    [UPDATE] Exemplo de Api de rastreio de encomendas

---
+++ src/Correios.NET.Api/Controllers/PackageTrackingController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Correios.NET.Api.Controllers
+{
+    [ApiController]
+    [Route("encomendas")]
+    public class PackageTrackingController : ControllerBase
+    {
+        private readonly Correios.NET.IServices _services;
+        private readonly ILogger<PackageTrackingController> _logger;
+
+        public PackageTrackingController(Correios.NET.IServices services, ILogger<PackageTrackingController> logger)
+        {
+            _services = services;
+            _logger = logger;
+        }
+
+        [Route("{code}")]
+        [HttpGet]
+        public async Task<Correios.NET.Models.Package> Get(string code)
+        {
+            return await _services.GetPackageTrackingAsync(code);
+        }        
+    }
+}
--- src/Correios.NET.Api/Controllers/ZipCodeController.cs
+++ src/Correios.NET.Api/Controllers/ZipCodeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Correios.NET.Exceptions;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Correios.NET.Api.Controllers
 {
