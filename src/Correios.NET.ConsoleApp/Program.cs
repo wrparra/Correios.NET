4987d076af5cfbfb312edf43bd18736919467312
Wellington Rocha Parra <wrparra@wrsoft.com.br>
2022-07-11 17:31:49 -0300

    FEAT: Ajustes e Samples

---
+++ src/Correios.NET.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+WriteLineWithColor("Correios.NET", ConsoleColor.Yellow);
+Console.WriteLine("1 - Consultar CEP");
+Console.WriteLine("2 - Consultar Encomenda");
+Console.ReadLine();
+
+
+void WriteLineWithColor(string value, ConsoleColor foreColor = ConsoleColor.White)
+{
+    var oldForeColor = Console.ForegroundColor;
+    Console.ForegroundColor = foreColor;
+    Console.WriteLine(value);
+    Console.ForegroundColor = oldForeColor;
+}
--- src/Correios.NET.WebApp/Program.cs
+++ src/Correios.NET.WebApp/Program.cs
@@ -1,6 +1,6 @@
 using Correios.NET;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); 
 builder.Services.AddScoped<ICorreiosService, CorreiosService>();
 
 var app = builder.Build();
